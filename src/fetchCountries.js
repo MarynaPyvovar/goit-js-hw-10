c6d2d6533c5c4c3b26868a5e73b4e73718bf69b5
MarynaPyvovar <marina18renkas@gmail.com>
2022-08-04 19:46:50 +0300

    feat index

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,44 +1,7 @@
-import Notiflix from 'notiflix';
-
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-
 export const fetchCountries = name => {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`)
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
     .then(response => response.json())
-    .then(data => {
-      if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        return;
-      }
-      console.log(data);
-
-      if (data.length < 10) {
-        return renderList(data);
-      }
-      if (data.length === 1) {
-        return renderCountryInfo(data[0]);
-      }
-    })
-    .catch(error => {
-      console.log(error);
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+    .catch(error => console.log(error));
 };
-
-function renderCountryInfo({
-  name: { official },
-  capital,
-  population,
-  flags: { svg },
-  languages,
-}) {}
-
-function renderList({ name: { official }, flags: { svg } }) {
-  const list = data
-    .map(item => `<li><svg><use src=${svg}></use></svg><p>${official}</p></li>`)
-    .join('');
-  countryList.insertAdjacentHTML(list);
-}
