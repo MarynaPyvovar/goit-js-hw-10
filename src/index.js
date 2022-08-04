dd149358ac35a776c9f5511945bf9665ff1a2b5c
MarynaPyvovar <marina18renkas@gmail.com>
2022-08-04 14:06:10 +0300

    feat
    add dependancies + start js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInput = document.querySelector('#search-box');
+// const countryList = document.querySelector('.country-list');
+
+searchInput.addEventListener('input', debounce(handleSearch, DEBOUNCE_DELAY));
+
+function handleSearch() {
+  const countryName = searchInput.value;
+  if (countryName === '') {
+  }
+  //   console.log(countryName);
+  const countryInfo = fetchCountries(countryName);
+  console.log(countryInfo);
+  //   countryList.insertAdjacentHTML(countryInfo);
+}
